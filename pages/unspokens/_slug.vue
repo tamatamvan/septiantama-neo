8f69f90232cae7357bc881f85f191d6eea593c42
tamatamvan <sfirst.tama@gmail.com>
2020-12-05 21:56:18 +0700

    refactor detail page with composition API

--- pages/unspokens/_slug.vue
+++ pages/unspokens/_slug.vue
@@ -1,24 +1,29 @@
-<script>
+<script lang="ts">
+import {
+  defineComponent,
+  useStatic,
+  useContext,
+  computed,
+} from '@nuxtjs/composition-api'
 import format from 'date-fns/format'
-export default {
-  async asyncData({ $content, params }) {
-    const article = await $content('unspokens', params.slug).fetch()
+
+export default defineComponent({
+  setup() {
+    const { $content, params } = useContext()
+    const slug = computed(() => params.value.slug)
+    const article = useStatic(
+      async (slug) => await $content('unspokens', slug).fetch(),
+      slug,
+      'article'
+    )
 
     return {
-      article: {
-        ...article,
-        publisedDate: format(new Date(article.date), 'LLL Do, yyyy - h:m bbbb'),
-      },
+      article: article.value,
+      format,
     }
   },
-}
+})
 </script>
-
-<style scoped>
-* {
-  font-family: 'Merriweather', serif;
-}
-</style>
 
 <style>
 .nuxt-content p {
@@ -42,7 +47,7 @@ export default {
     <div class="h-screen w-full md:fixed md:left-0 md:w-1/2">
       <img
         src="/unspoken.jpeg"
-        :alt="article.title"
+        :alt="article && article.title"
         class="poem-img h-screen w-full object-cover object-center"
       />
     </div>
@@ -53,12 +58,14 @@ export default {
         class="flex flex-col justify-center items-center h-full w-full text-white"
       >
         <h1 class="article-title text-2xl md:text-3xl italic bold">
-          {{ article.title }}
+          {{ article && article.title }}
         </h1>
         <br />
         <p>
           Published at:
-          <span class="block">{{ article.publisedDate }}</span>
+          <span class="block">{{
+            article && format(new Date(article.date), 'LLL Do, yyyy - h:m bbbb')
+          }}</span>
         </p>
       </div>
     </div>
