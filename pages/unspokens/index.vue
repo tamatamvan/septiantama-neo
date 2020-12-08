e75d896eb253b157f188065cf0a9eca0ad956ab0
tamatamvan <sfirst.tama@gmail.com>
2020-12-08 16:18:26 +0700

    initiate unspokens index page

---
+++ pages/unspokens/index.vue
@@ -0,0 +1,22 @@
+<script>
+// import format from 'date-fns/format'
+import PageTitle from '../../components/commons/PageTitle'
+export default {
+  components: {
+    PageTitle,
+  },
+  async asyncData({ $content }) {
+    const articles = await $content('unspokens').fetch()
+
+    return {
+      articles,
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="unspokens w-1/2 ml-auto mr-auto">
+    <PageTitle title="Unspoken Words" />
+  </div>
+</template>
