ace916029cf0b44a2a616704a103b585eebaf8c6
tamatamvan <sfirst.tama@gmail.com>
2020-12-08 16:17:43 +0700

    create PageTitle component

---
+++ components/commons/PageTitle.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    title: String,
+  },
+})
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold pt-8 pb-12">{{ title }}</h1>
+</template>
